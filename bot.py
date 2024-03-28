955affb834d48f24a144b6b0f8b97374b3842bb7
Filip Tichý <filiptichy888@gmail.com>
2024-03-28 11:27:43 +0100

    dm hello

--- bot.py
+++ bot.py
@@ -11,15 +11,13 @@ client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    print(f'{client.user.name} has connected to Discord!')
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
     )
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
 
 client.run(TOKEN)
