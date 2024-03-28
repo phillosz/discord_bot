91d2e3b7acca8c786371780aec9382a41087c6e4
Filip Tichý <filiptichy888@gmail.com>
2024-03-28 11:28:18 +0100

    .

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ async def on_ready():
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
+        f'{member.name}, test was succesful!'
     )
 
 client.run(TOKEN)
