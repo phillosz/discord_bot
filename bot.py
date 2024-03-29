a85c0747334f831defbb8beb7ff612e6ecb5d584
Filip Tichý <filiptichy888@gmail.com>
2024-03-29 11:41:46 +0100

    funictioning member list and newcomer dm

--- bot.py
+++ bot.py
@@ -7,17 +7,34 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
 
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+    
 @client.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(
-        f'{member.name}, test was succesful!'
+        f'Hi {member.name}, welcome to my Discord server!'
     )
-
+    
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+    
 client.run(TOKEN)
